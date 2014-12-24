38d37706676cb56f24b6fb88c4a088acb5fc7918
blibotics <blibotics@gmail.com>
2014-12-24 17:26:58 -0500

    Adding scroll on click, adjusting font sizes font devices, changed font stack

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,28 @@
 'use strict';
+console.log("fired");
 
 (function($) {
   $(document).ready(function() {
     // Add your jQuery code here
+
+    var $window = $(window),
+        $scrollDownBtn = $('#scroll-down'),
+        $currentSlide = $scrollDownBtn.closest('.slide'),
+        nextSlide = $currentSlide.next();
+
+    // console.log($nextSlideOffsetTop);
+
+    $scrollDownBtn.on('click', function(){
+      var nextSlideOffset = nextSlide.offset(),
+          nextSlideOffsetTop = nextSlideOffset.top;
+
+      // $window.scrollTop(nextSlideOffsetTop);
+
+      $('html, body').animate({
+          scrollTop: nextSlideOffsetTop
+        }, 500);
+
+    });
+
   });
 })(jQuery);
